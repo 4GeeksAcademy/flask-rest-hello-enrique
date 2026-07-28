2da0602991daee9d80d16ad80c5dcb925e659379
enriquetoscanoh94 <enriquetoscanoh94@icloud.com>
2026-07-28 22:45:22 +0000

    Add StarWars blog database models

--- src/models.py
+++ src/models.py
@@ -1,19 +1,69 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Boolean, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 db = SQLAlchemy()
 
+
 class User(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    email: Mapped[str] = mapped_column(
+        String(120), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
 
+    favorites = relationship('Favorite', backref='user', lazy=True)
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
-            # do not serialize the password, its a security breach
         }
+
+
+class Planet(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(120), nullable=False)
+    climate: Mapped[str] = mapped_column(String(120), nullable=False)
+    population: Mapped[str] = mapped_column(String(120), nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "climate": self.climate,
+            "population": self.population,
+        }
+
+
+class Character(db.Model):
+    id: Mapped[int] = mapped_column(primary_
+    name: Mapped[str]=mapped_column(String(120), nullable=False)
+    gender: Mapped[str]=mapped_column(String(120), nullable=False)
+    birth_year: Mapped[str]=mapped_column(
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "gender": self.gender,
+            "birth_year": self.birth_year,
+        }
+
+
+class Favorite(db.Model):
+    id: Mapped[int]=mapped_column(primary_
+    user_id: Mapped[int]=mapped_column(ForeignKey('user.id'), nullable=False)
+    planet_id: Mapped[int]=mapped_column(
+        ForeignKey('planet.id'), nullable=True)
+    character_id: Mapped[int]=mapped_column(
+        ForeignKey('character.id'), nullable=True)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "planet_id": self.planet_id,
+            "character_id": self.character_i
+        }
+        
